e521b507658c37a7cf543ce8cd5e9135b2cc4b86
Asish kumar Upadhyay <asishupadhyay24@gmail.com>
2021-03-21 13:40:05 +0530

    first

---
+++ client/src/components/views/LandingPage/sections/GridCard.js
@@ -0,0 +1,18 @@
+import  React from 'react';
+import {col} from 'antd';
+
+function GridCard(){
+    return (
+        <col lg={6} md={8} xs={24}>
+            <div style={{position:'relative'}}>
+                <a href>
+                    <img style={{width:'100%',height:'320px'}} alt src/>
+
+
+                </a>
+
+            </div>
+        
+        </col>
+    )
+}
