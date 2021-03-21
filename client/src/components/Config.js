cc595735da5e81f1e8eafbb6906a96fd95991f87
Asish kumar Upadhyay <asishupadhyay24@gmail.com>
2021-03-21 13:42:20 +0530

    first

---
+++ client/src/components/Config.js
@@ -0,0 +1,16 @@
+export const USER_SERVER = '/api/users';
+
+
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '80b76abd523ee3456ff3f9fc5ac8b002';
+
+
+export const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
+
+//Sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE = 'w1280'
+export const IMAGE_SIZE = 'w1280'
+
+// w92, w154, w185, w342, w500, w780, original
+export const POSTER_SIZE = 'w500'       
